eded78fac190fdeac8e40669a5f6e8366470be0e
Robin <robin@MacBook-Pro-von-Robin.local>
2026-03-06 06:18:32 +0100

    fix: remove leaflet css from layout

--- app/MapClient.tsx
+++ app/MapClient.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import "leaflet/dist/leaflet.css";
 import { useEffect, useMemo, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents, useMap } from "react-leaflet";
 import L from "leaflet";
